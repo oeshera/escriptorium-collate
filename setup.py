a36a6b9f2d319bee56e9b39e0d3db7286852378c
omeshera <osama.eshera@gmail.com>
2024-06-11 18:05:26 -0400

    Deployment prep

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='escriptorium-collate ',
-    version='0.1.0',
+    name="escriptorium-collate",
+    version="0.1.0",
     author="Osama Eshera",
     author_email="osama.eshera@gmail.com",
     description="A python package for collating eScriptorium documents.",
+    url="https://github.com/oeshera/escriptorium-collate",
     packages=find_packages(),
     install_requires=[
         "escriptorium_connector",
